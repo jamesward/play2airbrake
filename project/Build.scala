bfe90f4f17a7541fb696feafe3af347eadae1478
James Ward <james@jamesward.org>
2012-09-11 06:14:32 -0600

    bump version of play-airbrake

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "eu.teamon" %% "play-airbrake" % "0.1.0"
+      "eu.teamon" %% "play-airbrake" % "0.1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      resolvers += "teamon.eu repo" at "http://repo.teamon.eu"
+      resolvers += "scalajars.org repo" at "http://scalajars.org/repository"
     )
 
 }
